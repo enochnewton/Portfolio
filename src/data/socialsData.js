afe1f6c3dbc235176d0b03eec6b6caf99ea3c713
enochnewton <enochnewton2@gmail.com>
2024-01-28 17:42:39 +0300

    1st commit

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,14 +1,15 @@
 export const socialsData = {
-    github: 'https://github.com/',
-    facebook: 'https://www.facebook.com/',
-    linkedIn: 'https://www.linkedin.com/in',
-    instagram: 'https://www.instagram.com/',
-    codepen: 'https://codepen.io/',
-    twitter: 'https://twitter.com/',
-    reddit: 'https://www.reddit.com/user/',
-    blogger: 'https://www.blogger.com/',
-    medium: 'https://medium.com/@',
-    stackOverflow: 'https://stackoverflow.com/users/',
-    gitlab: 'https://gitlab.com/',
-    youtube: 'https://youtube.com/'
-}
+  github: "https://github.com/",
+  facebook: "https://www.facebook.com/",
+  linkedIn:
+    "https://www.linkedin.com/in/enoch-kathiomi-ba8449225?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3But%2F6WASqR9ydrT5K45FK3g%3D%3D",
+  instagram: "https://www.instagram.com/",
+  codepen: "https://codepen.io/",
+  twitter: "https://twitter.com/",
+  reddit: "https://www.reddit.com/user/",
+  blogger: "https://www.blogger.com/",
+  medium: "https://medium.com/@",
+  stackOverflow: "https://stackoverflow.com/users/",
+  gitlab: "https://gitlab.com/",
+  youtube: "https://youtube.com/",
+};
